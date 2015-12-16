9339b1e7c75a67774eb417eaccc4a84f37f06017
snlyf <root@sunahip.gob.ve>
2015-12-16 08:40:58 -0430

    update

---
+++ sunahipdb.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 03-11-2015 a las 08:33:19
+-- Versión del servidor: 5.5.46
+-- Versión de PHP: 5.4.45-0+deb7u2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `sunahipdb2`
+--
+CREATE DATABASE `sunahipdb2` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sunahipdb2`;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
